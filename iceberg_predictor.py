547f93bc0137831070107d75ae0ad4bda83a1b50
Hadi Tadayyon <hadi.tadayyon@gmail.com>
2018-01-07 22:36:12 -0500

    Update iceberg_predictor.py
    added hyper-parameter tuning using random search

--- iceberg_predictor.py
+++ iceberg_predictor.py
@@ -1,28 +1,44 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec 16 17:12:29 2017
+Created on Fri Jan  5 18:23:47 2018
 
 @author: admin
 """
 
+# This Python 3 environment comes with many helpful analytics libraries installed
+# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
+# For example, here's several helpful packages to load in 
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+# Input data files are available in the "../input/" directory.
+# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+
+# Any results you write to the current directory are saved as output.
+
 import pandas as pd 
 import numpy as np 
-from sklearn.model_selection import train_test_split
-np.random.seed(1337) # The seed I used - pick your own or comment out for a random seed. A constant seed allows for better comparisons though
-
-# Import Keras 
-from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
-from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten, Activation, Merge
-from keras.layers import Conv2D, MaxPooling2D
-from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau
-from keras.layers.normalization import BatchNormalization
-from keras.optimizers import Adam
+import time # to track elapsed time
+from sklearn.model_selection import train_test_split, RandomizedSearchCV
+from scipy.stats import randint as sp_randint
+from keras.wrappers.scikit_learn import KerasClassifier
 
 import os
 os.chdir("D:\Kaggle\Iceberg Cassification1\iceberg-classification")
+np.random.seed(1337) # The seed I used - pick your own or comment out for a random seed. A constant seed allows for better comparisons though
 
-df_train = pd.read_json('train.json') # this is a dataframe
+# Import Keras 
+import keras
+from keras.callbacks import ModelCheckpoint, Callback, EarlyStopping
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, Callback
+from keras.layers.normalization import BatchNormalization
+from keras.optimizers import Adam, SGD, RMSprop, Adagrad, Adadelta, Adamax, Nadam
+
+df_train = pd.read_json('../input/train.json') # this is a dataframe
 
 def get_scaled_imgs(df):
     imgs = []
@@ -34,9 +50,6 @@ def get_scaled_imgs(df):
         band_3 = band_1 + band_2 # plus since log(x*y) = log(x) + log(y)
         
         # Rescale
-#        a = (band_1 - band_1.mean()) / (band_1.max() - band_1.min())
-#        b = (band_2 - band_2.mean()) / (band_2.max() - band_2.min())
-#        c = (band_3 - band_3.mean()) / (band_3.max() - band_3.min())
         a = (band_1 - band_1.mean()) / (band_1.std())
         b = (band_2 - band_2.mean()) / (band_2.std())
         c = (band_3 - band_3.mean()) / (band_3.std())
@@ -49,13 +62,11 @@ Xtrain = get_scaled_imgs(df_train)
 Ytrain = np.array(df_train['is_iceberg'])
 
 df_train.inc_angle = df_train.inc_angle.replace('na',0)
-df_train.inc_angle = df_train.inc_angle.astype(float).fillna(0.0)
-X_angle_train = np.array(df_train.inc_angle)
+idx_tr = np.where(df_train.inc_angle>0)
 
 x_train, x_val, y_train, y_val = train_test_split(Xtrain, Ytrain, random_state = 1, train_size=0.75)
 
-
-def getModel():
+def getModel(optimizer='adam'):
     #Build keras model
     
     model=Sequential()
@@ -94,35 +105,62 @@ def getModel():
     # Output 
     model.add(Dense(1, activation="sigmoid"))
 
-    optimizer = Adam(lr=0.001, decay=0.0)
     model.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
     
     return model
 
-model = getModel()
-model.summary()
+#model = getModel()
+#model.summary()
 
-batch_size = 32
+model = KerasClassifier(build_fn=getModel, epochs=20, batch_size=10, verbose=2)
+# define the grid search parameters
+optimizer = ['SGD', 'RMSprop', 'Adagrad', 'Adadelta', 'Adam', 'Adamax', 'Nadam']
+
+param_dist = {"batch_size":sp_randint(20,50),
+              "optimizer":optimizer}
+
+grid = RandomizedSearchCV(estimator=model, param_distributions=param_dist, n_iter=10, verbose=2)
+
+#batch_size = 32
 earlyStopping = EarlyStopping(monitor='val_loss', patience=10, verbose=0, mode='min')
 mcp_save = ModelCheckpoint('.mdl_wts.hdf5', save_best_only=True, monitor='val_loss', mode='min')
 
-model.fit(x_train, y_train, batch_size=batch_size, 
-          epochs=20, verbose=1, 
-          callbacks=[earlyStopping, mcp_save], 
-          validation_data = (x_val, y_val))
-
-model.load_weights(filepath = '.mdl_wts.hdf5')
-scores = model.evaluate(x_val, y_val, verbose=0)
-print('Test loss:', scores[0])
-print('Test accuracy:', scores[1])
+grid_result = grid.fit(x_train, y_train, callbacks=[earlyStopping, mcp_save])
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+# Best: 0.877805 using {'batch_size': 48, 'optimizer': 'SGD'}
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+#0.876143 (0.013091) with: {'batch_size': 43, 'optimizer': 'Adamax'}
+#0.877805 (0.010774) with: {'batch_size': 48, 'optimizer': 'SGD'}
+#0.868662 (0.015282) with: {'batch_size': 27, 'optimizer': 'RMSprop'}
+#0.873649 (0.005124) with: {'batch_size': 27, 'optimizer': 'Adagrad'}
+#0.857024 (0.017076) with: {'batch_size': 38, 'optimizer': 'Adagrad'}
+#0.861180 (0.011214) with: {'batch_size': 40, 'optimizer': 'SGD'}
+#0.857855 (0.017751) with: {'batch_size': 29, 'optimizer': 'Nadam'}
+#0.865337 (0.024006) with: {'batch_size': 42, 'optimizer': 'Adagrad'}
+#0.873649 (0.005124) with: {'batch_size': 43, 'optimizer': 'SGD'}
+#0.861180 (0.014730) with: {'batch_size': 21, 'optimizer': 'Adadelta'}
+#
+#model.fit(x_train, y_train, batch_size=batch_size, 
+#          epochs=20, verbose=1, 
+#          callbacks=[earlyStopping, mcp_save], 
+#          validation_data = (x_val, y_val))
+#
+#model.load_weights(filepath = '.mdl_wts.hdf5')
+#scores = model.evaluate(x_val, y_val, verbose=0)
+#print('Test loss:', scores[0])
+#print('Test accuracy:', scores[1])
 
 #Now load the test data and predict the labels
-df_test = pd.read_json('test.json')
-df_test.inc_angle = df_test.inc_angle.replace('na',0)
-Xtest = (get_scaled_imgs(df_test))
-pred_test = model.predict(Xtest)
+#df_test = pd.read_json('../input/test.json')
+#df_test.inc_angle = df_test.inc_angle.replace('na',0)
+#Xtest = (get_scaled_imgs(df_test))
+#pred_test = model.predict(Xtest)
+#
+#submission = pd.DataFrame({'id': df_test["id"], 'is_iceberg': pred_test.reshape((pred_test.shape[0]))})
+#print(submission.head(10))
 
-submission = pd.DataFrame({'id': df_test["id"], 'is_iceberg': pred_test.reshape((pred_test.shape[0]))})
-print(submission.head(10))
-
-submission.to_csv('submission.csv', index=False)
+#submission.to_csv('submission.csv', index=False)
